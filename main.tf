b16b12b0d9aa7ed95e6075a81a11c71de5ca6b28
kitsanaphon1 <123629506+kitsanaphon1@users.noreply.github.com>
2025-07-03 09:38:02 +0700

    commit

--- main.tf
+++ main.tf
@@ -65,6 +65,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+    security_rule {
+    name                       = "Allow-3000"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # 🌐 Public IP
